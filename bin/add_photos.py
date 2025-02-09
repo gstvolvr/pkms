d3cbde78942101c3e7c3fd1e43ab286db3198f2f
Gustavo A. Oliver <g.antonio.oliver@gmail.com>
2025-02-09 07:53:53 -0500

    widen photos

--- bin/add_photos.py
+++ bin/add_photos.py
@@ -12,7 +12,7 @@ Add photos paths to markdown files
 PHOTO_DATE_DIR = f"{util.PHOTOS_PATH}/{{year}}/{{month}}/{{date}}"
 
 
-def get_photos(path: str) -> List[str]:
+def get_photo_files(path: str, postfixes: List[str] = ['jpeg', 'jpg']) -> List[str]:
     """
     path follows the pattern like: ${USER}/{VAULT}/pensieve/day/2018/12/181212.md
     """
@@ -21,26 +21,27 @@ def get_photos(path: str) -> List[str]:
 
     for root, dirs, files in os.walk(PHOTO_DATE_DIR.format(year=year, month=month, date=date)):
         for file in files:
-            if file.lower().endswith('jpeg') or file.lower().endswith('jpg'):
+            if file.lower().split('.')[-1] in postfixes:
                 # TODO: use user directory
                 yield '/'.join(['/Users', 'home', 'photos', year, month, date, file])
 
 
 def markdown_grid(paths: List[str]) -> str:
-    image_tags = '\n'.join([f'<img src="file://{p}" width="100"/>' for p in paths])
+    image_tags = '\n'.join([f'<img src="file://{p}" width="300"/>' for p in paths])
     return f"""<p align="center">
 {image_tags}
 </p>
     """
 
 def markdown_photo(path: str) -> str:
-    return f"![200](file://{path})\n"
+    return f"![400](file://{path})\n"
 
 
 def main():
     existing_files = glob.glob(f'{util.PENSIEVE_PATH}/*/*/*')
+    n_updates = 0
     for i, path in enumerate(existing_files):
-        paths = list(get_photos(path))
+        paths = list(get_photo_files(path))
         random.shuffle(paths)
 
         if len(paths) == 0:
@@ -54,8 +55,10 @@ def main():
 
         with open(path, 'a') as w:
             print(f'appending: {path}')
+            n_updates += 1
             w.write('\n')
             w.write(grid)
+    print(f'Updated {n_updates} files.')
 
 
 if __name__ == '__main__':
